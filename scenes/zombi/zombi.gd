54ae1b6bdc3f06ee87adb2a59e7093955b0b71a7
Vova-Postnyi <v6022915@gmail.com>
2025-06-05 21:02:10 +0300

    создал зомби

---
+++ scenes/zombi/zombi.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var max_speed = 80
+
+func _ready():
+	pass
+	
+	
+	
+func _process(delta):
+	var direction = get_direction_to_player()
+	velocity = max_speed * direction
+	move_and_slide()	
+	
+		
+			
+func get_direction_to_player():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player != null:
+		return (player.global_position - global_position).normalized()
+	return Vector2(0,0)	 
