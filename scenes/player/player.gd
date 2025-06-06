ef1c9b379a28f13d0cdb79245cc9bdde37dd8b33
Nazar Zozulya <likeemangames@gmail.com>
2025-06-06 13:39:30 +0300

    сделал анимацию получения урона и смерти

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @export var stats: StatBlock
 var player_direction = false
+var is_damaged = false
+var is_hit_anim_playing = false
+var dead_started = false
 
 func _ready():
 	if stats == null:
@@ -19,33 +22,63 @@ func _process(delta):
 	update_animation(direction)
 	update_velocity(direction)
 	update_direction()
+
+	# Проверка смерти и запуск анимации один раз
+	if not dead_started and stats.health <= 0:
+		dead_started = true
+		die()
+
 	$animations.flip_h = player_direction
 	move_and_slide()
 	$player_ui/Label.text = str(stats.health)
+
 
 func get_direction() -> Vector2:
 	var movement_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var movement_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(movement_x, movement_y).normalized()
 
+
 func update_animation(direction: Vector2) -> void:
+	if is_hit_anim_playing or dead_started:
+		return
+	
 	if direction != Vector2.ZERO:
 		$animations.play("run")
 	else:
 		$animations.play("idle")
-		
-		
+
 
 func update_velocity(direction: Vector2) -> void:
 	velocity = direction * stats.speed
+
 
 func update_direction() -> void:
 	if velocity.x < 0:
 		player_direction = true
 	elif velocity.x > 0:
 		player_direction = false
-		
+
+
 func take_damage(amount: int) -> void:
+	if dead_started:
+		return 
+
 	stats.take_damage(amount)
+	is_damaged = true
+	is_hit_anim_playing = true
 	$animations.play("hit")
-	print("Игрок получил урон, здоровье:", stats.health)
+
+	async_func_reset_hit()
+
+
+func async_func_reset_hit() -> void:
+	await get_tree().create_timer(0.3).timeout
+	is_hit_anim_playing = false
+	is_damaged = false
+
+
+func die() -> void:
+	$animations.play("die")
+	await $animations.animation_finished
+	get_tree().reload_current_scene()
--- scenes/zombi/zombi.gd
+++ scenes/zombi/zombi.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var max_speed = 80
+var damage = 4
 
 func _ready():
 	pass
@@ -10,7 +11,7 @@ func _ready():
 func _process(delta):
 	var direction = get_direction_to_player()
 	velocity = max_speed * direction
-	move_and_slide()	
+	move_and_slide()
 	
 		
 			
@@ -19,3 +20,8 @@ func get_direction_to_player():
 	if player != null:
 		return (player.global_position - global_position).normalized()
 	return Vector2(0,0)	 
+
+
+func _player_entered(body: Node2D) -> void:
+	if body.get_tree().get_first_node_in_group("player"):
+		body.take_damage(damage)
