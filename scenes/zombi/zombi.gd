bfd0fe05f5ebc19b78f1c91515e9111f7a3edd41
Vova-Postnyi <v6022915@gmail.com>
2025-06-11 14:45:42 +0300

    gay 2

--- scenes/zombi/zombi.gd
+++ scenes/zombi/zombi.gd
@@ -1,21 +1,29 @@
 extends CharacterBody2D
 
-var max_speed = 80
+@export var max_speed: float = 80
+@export var max_health: int = 100
+var current_health: int
+
+var player: Node2D
 
 func _ready():
-	pass
-	
-	
-	
+	current_health = max_health
+	player = get_tree().get_first_node_in_group("player") as Node2D
+	update_health_bar()
+
 func _process(delta):
-	var direction = get_direction_to_player()
-	velocity = max_speed * direction
-	move_and_slide()	
-	
-		
-			
-func get_direction_to_player():
-	var player = get_tree().get_first_node_in_group("player") as Node2D
-	if player != null:
-		return (player.global_position - global_position).normalized()
-	return Vector2(0,0)	 
+	if player:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = max_speed * direction
+		move_and_slide()
+
+func take_damage(amount: int):
+	current_health = max(current_health - amount, 0)
+	update_health_bar()
+	if current_health <= 0:
+		queue_free()  # враг умирает
+
+func update_health_bar():
+	var bar = $HealthBar as ProgressBar
+	if bar:
+		bar.value = current_health
